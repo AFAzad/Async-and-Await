16114c37cdebd2803fd523cc0e88d93302b338ee
Abul Fajle Azad <abulfajle405@gmail.com>
2022-08-18 22:07:48 +0530

    2nd commit

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-async function test(){
-    return "Hello";
+async function test() {
+  return "Hello";
 }
 
-test().then((result)=>{
-    console.log(result);
- 
+test().then((result) => {
+  console.log(result);
 });
 
 test();
 
+// basicllay await function works on async fnction
 
-// basicllay await function works on async fnction 
-
-
-async function greet(){
-    console.log("Good Noon");
-    const response = await fetch("json/student-data.json");
-    const students = await response.json();
-    return students;
-    
+async function greet() {
+  console.log("Good Noon");
+  const response = await fetch("json/student-data1.json");
+  const students = await response.json();
+  return students;
 }
 
 console.log("Good Morning");
@@ -26,3 +22,36 @@ let a = greet();
 console.log("Good Ningt");
 console.log("Good bye");
 console.log(a);
+
+
+greet().then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
+/// fetching file of json
+
+async function test() {
+  console.log("2 : Message");
+  const response = await fetch("/student_data.json");
+  console.log("3 : Message");
+  const students = await response.json();
+
+  return students;
+}
+
+ console.log("1: Message");
+let x = test();
+console.log("4 : Message");
+console.log(x);
+
+test()
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
